a7bb82da95504a5e9ba719001cac487aa3ae87b3
Keagan McClelland <keagan.mcclelland@gmail.com>
2021-06-24 13:21:24 -0600

    adds properties logic to entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,35 +2,34 @@
 
 export HOST_IP=$(ip -4 route list match 0/0 | awk '{print $3}')
 
-cat /root/start9/config.yaml
-echo first
+#properties emission
+echo 'version: 2' > /root/start9/stats.yaml
+echo 'data:' >> /root/start9/stats.yaml
 PASS=$(yq e ".master-password" /root/start9/config.yaml)
-echo second
 yq -n e ".masterPassword = \"$PASS\"" > /root/accounts.yaml
 NUM_ACCOUNTS=$(yq e ".accounts | length" /root/start9/config.yaml)
-echo $NUM_ACCOUNTS Accounts
 for i in $(seq 0 $(($NUM_ACCOUNTS-1)))
 do
 	TYPE=$(yq e ".accounts[$i].connection-settings.type" /root/start9/config.yaml)
+	NAME=$(yq e ".accounts[$i].name" /root/start9/config.yaml)
+	ACCOUNT_PASS=$(yq e ".accounts[$i].password" /root/start9/config.yaml)
+	if [[ $ACCOUNT_PASS -ne "null" ]]
+	then
+		yq -i e ".accounts[$i].password = $ACCOUNT_PASS" /root/accounts.yaml
+		echo "  $NAME Password:"
+		echo "    value: \"$ACCOUNT_PASS\"" >> /root/start9/stats.yaml
+	else
+		echo "  $NAME Password: \"$PASS\"" >> /root/start9.yaml
+	fi
+	echo '    type: string'
+	echo "    description: \"Password to use with the account \"$NAME\""
+	echo '    copyable: true'
+	echo '    qr: false'
+	echo '    masked: true'
 	if [[ $TYPE -eq "internal" ]]
 	then
-		echo 1
-		NAME=$(yq e ".accounts[$i].name" /root/start9/config.yaml)
-		echo 2
 		URL="$(yq e ".accounts[$i].connection-settings.address" /root/start9/config.yaml)":10009
-		echo 3
 		yq -i e ".accounts[$i] = {\"name\":\"$NAME\", \"serverUrl\":\"$URL\", \"certificatePath\":\"/root/start9/public/lnd/tls.cert\", \"macaroonPath\":\"/root/start9/public/lnd/admin.macaroon\" }" /root/accounts.yaml
-		echo 4
-		ACCOUNT_PASS=$(yq e ".accounts[$i].password" /root/start9/config.yaml)
-		echo 5
-		echo $ACCOUNT_PASS
-		if [[ $ACCOUNT_PASS -ne "null" ]]
-		then
-			echo 6
-			yq -i e ".accounts[$i].password = $ACCOUNT_PASS" /root/accounts.yaml
-			echo 7
-		fi
-		echo 8
 	elif [[ $TYPE -eq "external" ]]
 	then
 		echo "Account $i is external"
