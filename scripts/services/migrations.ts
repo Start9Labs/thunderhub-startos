5fe6ef1a63c93c1495a29dec4b51228e6491561c
Dread <dread@start9labs.com>
2022-08-01 18:09:30 -0400

    remove unused migration code

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -1,24 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-  .fromMapping(
-    {
-      "0.13.14": {
-        up: compat.migrations.updateConfig(
-          (config) => {
-            return config;
-          },
-          true,
-          { version: "0.13.14", type: "up" },
-        ),
-        down: compat.migrations.updateConfig(
-          (config) => {
-            return config;
-          },
-          true,
-          { version: "0.13.14", type: "down" },
-        ),
-      },
-    },
-    "0.13.14",
-  );
+  .fromMapping({}, "0.13.14" );
