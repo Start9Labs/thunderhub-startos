a3acdcd3b84361c3912ee3a634c0906244012396
Dread <34528298+islandbitcoin@users.noreply.github.com>
2022-12-07 15:20:01 -0500

    v0.13.16 with x86 architecture (#24)
    Co-authored-by: islandbitcoin <dread@start9.com>

--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-  .fromMapping({}, "0.13.14" );
+  .fromMapping({}, "0.13.16" );
