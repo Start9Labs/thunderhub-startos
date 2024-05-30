6f3c3924384e3d9fa3fcd27d8b64a7f9a214fade
Mariusz Kogen <k0gen@pm.me>
2024-05-30 22:34:17 +0200

    fix healthceck on slower devices

--- scripts/procedures/healthChecks.ts
+++ scripts/procedures/healthChecks.ts
@@ -1,5 +1,19 @@
-import { types as T, healthUtil } from "../deps.ts";
+import { types as T, ok, error, guardDurationAboveMinimum } from '../deps.ts';
+
+const url = 'http://thunderhub.embassy:3000';
 
 export const health: T.ExpectedExports.health = {
-  "web-ui": healthUtil.checkWebUrl("http://thunderhub.embassy:3000")
-}
+  async 'web-ui'(effects, duration) {
+    const value = guardDurationAboveMinimum({ duration, minimumTime: 25_000 });
+    if (value) {
+      return value;
+    }
+    try {
+      await effects.fetch(url);
+      return ok;
+    } catch(e) {
+      console.warn(e);
+      return error('Can not reach service');
+    }
+  },
+};
